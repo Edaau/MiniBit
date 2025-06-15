b02fdca4b72ce631b05c1bc1a513451f928b01fc
brennojpg <brennoluiz20@gmail.com>
2025-06-15 00:33:48 -0300

    teste

---
+++ scripts/start_tracker.cmd
@@ -0,0 +1,11 @@
+start_tracker.cmd:
+
+@echo off
+echo Iniciando Tracker + Seeder...
+
+set PYTHONPATH=..
+
+python "D:\downloads google\SD\MiniBit\tracker\Tracker.py" 192.168.0.62 8000 D:\livro_vitima.pdf
+
+pause
+
